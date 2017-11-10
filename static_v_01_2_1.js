ff8e4d7a57479f316022ee31f1290ab9a04eaaa2
Oleg Ivashko <Oleg.Ivashko@yandex.ua>
2017-11-10 12:01:13 +0200

    Update and rename static_v_01_2.js to static_v_01_2_1.js

--- static_v_01_2_1.js
+++ static_v_01_2_1.js
@@ -18,7 +18,7 @@ BlStatic = (function() {
 
     for (idx = 0; idx < sc.length; idx++) {
         s = sc.item(idx);
-        if (s.src && s.src.includes('static_v_01_2.js')) {
+        if (s.src && s.src.includes('static_v_01_2_1.js')) {
             var url = new URL(s.src);
             conf_token  = url.searchParams.get('token');
             conf_host = url.searchParams.get('host');
